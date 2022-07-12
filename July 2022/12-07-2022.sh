35e3a3237f660786fc2b595fcb362fbd4ac47aa9
ishantpratapsingh <67059522+ishantpratapsingh@users.noreply.github.com>
2022-07-12 09:28:41 +0530

    Update 12-07-2022.sh

--- July 2022/12-07-2022.sh
+++ July 2022/12-07-2022.sh
@@ -1,4 +1,4 @@
-# Write a shell script to clear the /var/logs file.
+# Write a shell script to clear the /var/logs file. Make a note that we are not deleting /var/log file but instead cleaning it's contents.
 #!/bin/bash
 
 cd /var/log
